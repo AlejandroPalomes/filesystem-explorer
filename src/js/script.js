355c0ae017f0567c90b0a713871abc92963d9e72
Alex Palomes <alejandropalomes@hotmail.com>
2020-07-14 17:26:53 +0200

    dir opens

--- src/js/script.js
+++ src/js/script.js
@@ -14,26 +14,58 @@ axios({
 }).then((response)=>iterateFolders(response.data));
 
 
-function iterateFolders(folder){
+function iterateFolders(folder, parent){
     let key = Object.keys(folder);
     key.forEach(e => {
         if(folder[e].type === 'directory'){
-            //console.log(typeof folder[e].content)
+            let newId = folder[e].path.replace(/\//g, '_').replace(/\./g, '');
             let contents;
             hasContent = false;
-
             if (typeof folder[e].content == 'object') contents = Object.keys(folder[e].content);
             if (contents.length) hasContent = true;
 
             let li = document.createElement('li');
-            li.className = `nav-item ${(hasContent) ? 'dropdwon' : ''}`;
-            li.innerHTML = `
-                <a class="nav-link active" data-path="${folder[e].path}" ${(hasContent) ? ' data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"' : 'href="#"'}>${folder[e].name}</a>
-            `;
+            let li2 = document.createElement('li');
+            li.className = 'nav-item';
+            li2.className = 'nav-item';
 
+            if(!parent){
+                li.className = `nav-item ${(hasContent) ? 'dropdwon' : ''}`;
+                li.innerHTML = `
+                    <a class="nav-link" data-path="${folder[e].path}" ${(hasContent) ? ' data-toggle="collapse" href="#'+ newId +'" role="button" aria-expanded="false" aria-controls="testColl"' : 'href="#"'}>${folder[e].name}</a>
+                `;
 
-            document.querySelector('#sideMenu').append(li);
-            iterateFolders(folder[e].content);
+                li2.innerHTML = `
+                <ul class="collapse" id="${newId}" data-father="${folder[e].path}"><ul>
+                `
+                document.querySelector('#sideMenu').append(li);
+                if(hasContent) document.querySelector('#sideMenu').append(li2);
+            }else{
+                let li3 = document.createElement('li');
+                if(hasContent){
+                    li.className = `nav-item dropdwon pl-4`;
+                    li.innerHTML = `
+                        <a class="nav-link" data-path="${folder[e].path}" ${(hasContent) ? ' data-toggle="collapse" href="#' + newId + '" role="button" aria-expanded="false" aria-controls="testColl"' : 'href="#"'}>${folder[e].name}</a>
+                    `;
+
+                    let li3 = document.createElement('li');
+                    li3.innerHTML = `
+                    <ul class="collapse" id="${newId}" data-father="${folder[e].path}"><ul>
+                    `
+                    document.querySelector(`[data-father="${parent}"]`).append(li);
+
+                    if(hasContent) document.querySelector('#sideMenu').append(li3);
+                }else{
+                    li3.className = 'dropdown-item pl-5';
+                    li3.innerHTML = folder[e].name;
+                    li3.dataset['path'] = folder[e].path;
+                    document.querySelector(`[data-father="${parent}"]`).append(li3);
+                }
+            }
+            // if(document.querySelector('#sideMenu').lastChild.querySelector('a').dataset['path'] + '/' + folder[e].name === folder[e].path) console.log(folder[e].name + 'is a child')
+            // console.log(document.querySelector('#sideMenu').lastChild.querySelector('a').dataset['path'] + '/' + folder[e].name);
+            // console.log(document.querySelector(`[data-path="${folder[e].path}"]`));
+            iterateFolders(folder[e].content, folder[e].path);
         }else{
             //console.log(folder[e].name);
         }
