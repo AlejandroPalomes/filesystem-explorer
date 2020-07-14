b9e0e1aa1e4d3f1ab2f641bc741cb38ca2e7a54e
Alex Palomes <alejandropalomes@hotmail.com>
2020-07-14 21:33:16 +0200

    functional folder collapsible

--- src/js/script.js
+++ src/js/script.js
@@ -15,6 +15,7 @@ axios({
 
 
 function iterateFolders(folder, parent){
+    // document.querySelector('#sideMenu').innerHTML = '';
     let key = Object.keys(folder);
     key.forEach(e => {
         if(folder[e].type === 'directory'){
@@ -27,49 +28,57 @@ function iterateFolders(folder, parent){
             let li = document.createElement('li');
             let li2 = document.createElement('li');
             li.className = 'nav-item';
+            li.dataset['test'] = newId;
             li2.className = 'nav-item';
 
             if(!parent){
                 li.className = `nav-item ${(hasContent) ? 'dropdwon' : ''}`;
                 li.innerHTML = `
-                    <a class="nav-link" data-path="${folder[e].path}" ${(hasContent) ? ' data-toggle="collapse" href="#'+ newId +'" role="button" aria-expanded="false" aria-controls="testColl"' : 'href="#"'}>${folder[e].name}</a>
+                    <a class="nav-link" data-path="${folder[e].path}" ${(hasContent) ? ' data-toggle="collapse" href="#'+ newId +'" role="button" aria-expanded="false" aria-controls="'+ newId +'"' : 'href="#"'}>${folder[e].name}</a>
                 `;
 
+                // li2.innerHTML = `
+                // <ul class="collapse" id="${newId}" data-father="${folder[e].path}"></ul>
+                // `
+                li2.id = newId;
+                li2.className = 'collapse';
                 li2.innerHTML = `
-                <ul class="collapse" id="${newId}" data-father="${folder[e].path}"><ul>
+                <ul data-father="${folder[e].path}"></ul>
                 `
                 document.querySelector('#sideMenu').append(li);
                 if(hasContent) document.querySelector('#sideMenu').append(li2);
             }else{
                 let li3 = document.createElement('li');
+                li3.dataset['test'] = newId;
+
                 if(hasContent){
                     li.className = `nav-item dropdwon pl-4`;
                     li.innerHTML = `
-                        <a class="nav-link" data-path="${folder[e].path}" ${(hasContent) ? ' data-toggle="collapse" href="#' + newId + '" role="button" aria-expanded="false" aria-controls="testColl"' : 'href="#"'}>${folder[e].name}</a>
+                        <a class="nav-link" data-path="${folder[e].path}" ${(hasContent) ? ' data-toggle="collapse" href="#' + newId + '" role="button" aria-expanded="false" aria-controls="'+ newId +'"' : 'href="#"'}>${folder[e].name}</a>
                     `;
 
-                    let li3 = document.createElement('li');
+                    li3.id = newId;
+                    li3.className = 'collapse';
+                    // li3.innerHTML = `
+                    // <ul class="collapse" id="${newId}" data-father="${folder[e].path}"></ul>
+                    // `
                     li3.innerHTML = `
-                    <ul class="collapse" id="${newId}" data-father="${folder[e].path}"><ul>
+                    <ul data-father="${folder[e].path}"></ul>
                     `
                     document.querySelector(`[data-father="${parent}"]`).append(li);
-
-                    if(hasContent) document.querySelector('#sideMenu').append(li3);
+                    document.querySelector(`[data-father="${parent}"]`).append(li3);
+                    // document.querySelector('#sideMenu').append(li3);
                 }else{
                     li3.className = 'dropdown-item pl-5';
-                    li3.innerHTML = folder[e].name;
-                    li3.dataset['path'] = folder[e].path;
+                    li3.innerHTML = `<a class="nav-link" data-path="${folder[e].path}" href="#">${folder[e].name}</a>`;
+                    // li3.dataset['path'] = folder[e].path;
                     document.querySelector(`[data-father="${parent}"]`).append(li3);
                 }
             }
-            // if(document.querySelector('#sideMenu').lastChild.querySelector('a').dataset['path'] + '/' + folder[e].name === folder[e].path) console.log(folder[e].name + 'is a child')
-            // console.log(document.querySelector('#sideMenu').lastChild.querySelector('a').dataset['path'] + '/' + folder[e].name);
-            // console.log(document.querySelector(`[data-path="${folder[e].path}"]`));
             iterateFolders(folder[e].content, folder[e].path);
         }else{
             //console.log(folder[e].name);
         }
         // let folder = new Directory(folder[e]);
-        // console.log(folder)
     });
 }
