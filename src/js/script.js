211f4adaa20b9e3c1611a8c80adbf147d1dba7b5
Alex Palomes <alejandropalomes@hotmail.com>
2020-07-16 18:05:28 +0200

    drag and drop fully functional

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,45 @@ const renameInp = document.querySelector('#renameInput');
 const optionsBtn = document.querySelector('#optionsButton');
 const createFolderBtn = document.querySelector('#createFolderBtn');
 const uploadFileBtn = document.querySelector('#uploadFileConfirmBtn');
+const dropArea = document.querySelector('#contentWindow');
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)
+});
+
+function preventDefaults (e) {
+    e.preventDefault();
+    e.stopPropagation();
+};
+
+['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false)
+  })
+  
+  ;['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false)
+  })
+  
+  function highlight(e) {
+    dropArea.classList.add('highlight')
+  }
+  
+  function unhighlight(e) {
+    dropArea.classList.remove('highlight')
+  }
+
+  dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+  let dt = e.dataTransfer
+  let files = dt.files
+
+  handleFiles(files)
+}
+
+function handleFiles(files) {
+    ([...files]).forEach(uploadFile)
+}
 
 document.querySelector('body').addEventListener('click', e => {
     if (!e.target.classList.contains('rClickOption')) contextMenu.classList.add('d-none');
@@ -473,20 +512,6 @@ function uploadFile(file) {
         //handle error
         console.log('response');
     });
-
-
-    // $.ajax({
-    //     url: 'src/php/uploadFile.php',
-    //     type: 'POST',
-    //     processData: false, // important
-    //     contentType: false, // important
-    //     // dataType : 'json',
-    //     data: form,
-    //     success:function(data)
-    // {
-    //  console.log(data);
-    // }
-    // });
 }
 
 function showPreview(path) {
